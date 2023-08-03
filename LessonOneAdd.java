45d0418893f98b58e17ce77082d2e43756ceb6b6
Vladislav <141247851+losikvladislav@users.noreply.github.com>
2023-08-03 12:05:39 +0300

    Use method println, printf, Scanner

---
+++ LessonOneAdd.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LessonOneAdd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello. Input your name");
+        String name = in.nextLine();
+        System.out.printf("hello %s", name);
+    }
+}
